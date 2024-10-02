552be7e0e0f7ec807c9914e5a57f9b5c99424696
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2024-10-02 21:45:07 +0530

    Update misc.py

--- AlexaMusic/misc.py
+++ AlexaMusic/misc.py
@@ -55,17 +55,20 @@ def dbb():
 
 async def sudo():
     global SUDOERS
-    sudoers.add(config.OWNER_ID)
+    SUDOERS.add(config.OWNER_ID)
     sudoersdb = mongodb.sudoers
-    sudoers_data = await sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = set(sudoers_data["sudoers"] if sudoers_data else [])
-    if sudoers != set(sudoers_data.get("sudoers", [])):
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
         await sudoersdb.update_one(
             {"sudo": "sudo"},
             {"$set": {"sudoers": sudoers}},
             upsert=True,
         )
-    SUDOERS.update(sudoers)
+    if sudoers:
+        for user_id in sudoers:
+            SUDOERS.add(user_id)
     LOGGER(__name__).info(f"Sudoers Loaded.")
 
 
