ed7e3d9c03451d1b4503e3477ff9f607d8c72ed3
Silahanim21 <turkcell63632@gmail.com>
2024-11-09 18:08:59 +0300

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -44,14 +44,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             )
         ],
         [
-InlineKeyboardButton(
-                text=_["PL_B_3"],
-                callback_data=f"PanelMarkup {videoid}|{chat_id}",
-
-             )
-        ],
-        [
- InlineKeyboardButton(
+ 
+           InlineKeyboardButton(
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             )
