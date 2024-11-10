dbebce8a092e19d25685827471b1e8fc48b7f6df
Silahanim21 <turkcell63632@gmail.com>
2024-11-10 12:14:16 +0300

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -58,6 +58,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         ] 
 InlineKeyboardButton(text=_["CLOSE_BUTTON"],callback_data="close"),
+           ),
         ],
     ]
     return buttons
