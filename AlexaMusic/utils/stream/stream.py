11e656852ddb35f6251c56128678bc9d86bf3f36
Silahanim21 <turkcell63632@gmail.com>
2024-11-22 22:44:06 +0300

    Update stream.py

--- AlexaMusic/utils/stream/stream.py
+++ AlexaMusic/utils/stream/stream.py
@@ -32,7 +32,6 @@ from AlexaMusic.utils.inline.play import stream_markup, queue_markup, telegram_m
 from AlexaMusic.utils.inline.playlist import close_markup
 from AlexaMusic.utils.pastebin import Alexabin
 from AlexaMusic.utils.stream.queue import put_queue, put_queue_index
-from AlexaMusic.utils.thumbnails import gen_thumb, gen_qthumb
 
 
 async def stream(
@@ -116,10 +115,9 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                # theme = await check_theme(chat_id)
-                img = await gen_thumb(vidid)
+                # theme = await check_theme(chat_id
                 button = stream_markup(_, vidid, chat_id)
-                run = await app.send_photo(
+                run = await App.send_message (
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
@@ -143,7 +141,7 @@ async def stream(
                 car = msg
             carbon = await Carbon.generate(car, randint(100, 10000000))
             upl = close_markup(_)
-            return await app.send_photo(
+            return await App.send_message (
                 original_chat_id,
                 photo=carbon,
                 caption=_["playlist_18"].format(position, link),
@@ -205,7 +203,6 @@ async def stream(
                 forceplay=forceplay,
             )
             # theme = await check_theme(chat_id)
-            img = await (vidid)
             button = stream_markup(_, vidid, chat_id)
             try:
                 run = await App.send_message(
@@ -261,7 +258,7 @@ async def stream(
                 forceplay=forceplay,
             )
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await App.send_message(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
                 caption=_["stream_3"].format(title, duration_min, user_name),
@@ -311,7 +308,7 @@ async def stream(
             if video:
                 await add_active_video_chat(chat_id)
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await App.send_message (
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
                 caption=_["stream_4"].format(title, link, duration_min, user_name),
@@ -369,9 +366,8 @@ async def stream(
                 forceplay=forceplay,
             )
             # theme = await check_theme(chat_id)
-            img = await gen_thumb(vidid)
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await App.send_message(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
@@ -424,7 +420,7 @@ async def stream(
                 forceplay=forceplay,
             )
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await App.send_message (
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
                 caption=_["stream_2"].format(user_name),
